0edb04e817323c987dc7b5a025dca409c58497fc
Kenny Workman <kennyworkman@sbcglobal.net>
2022-05-20 22:38:18 -0700

    feat: in container config

---
+++ in_container.mk
@@ -0,0 +1,20 @@
+SERIALIZED_PB_OUTPUT_DIR := /tmp/output
+
+.PHONY: clean
+clean:
+	rm -rf $(SERIALIZED_PB_OUTPUT_DIR)/*
+
+$(SERIALIZED_PB_OUTPUT_DIR): clean
+	mkdir -p $(SERIALIZED_PB_OUTPUT_DIR)
+
+.PHONY: serialize
+serialize: $(SERIALIZED_PB_OUTPUT_DIR)
+	pyflyte --config /root/flytekit.config serialize workflows -f $(SERIALIZED_PB_OUTPUT_DIR)
+
+.PHONY: register
+register: serialize
+	flytectl register file --admin.clientId ${FLYTE_CLIENT_ID} --admin.clientSecretLocation ${FLYTE_SECRET_PATH} --admin.endpoint ${FLYTE_ADMIN_ENDPOINT} --admin.insecure -p ${PROJECT} -d development --version ${VERSION} $(SERIALIZED_PB_OUTPUT_DIR)/*
+
+.PHONY: test
+test:
+	pytest test.py
