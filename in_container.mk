9339eeddeae68010938b8c87ce9222706ba9982f
Rahul Desai <rahul@latch.bio>
2024-11-19 16:02:54 -0800

    add support for passing in custom package names

--- in_container.mk
+++ in_container.mk
@@ -9,7 +9,7 @@ $(SERIALIZED_PB_OUTPUT_DIR): clean
 
 .PHONY: serialize
 serialize: $(SERIALIZED_PB_OUTPUT_DIR)
-	pyflyte --config /root/flytekit.config serialize workflows -f $(SERIALIZED_PB_OUTPUT_DIR)
+	pyflyte --config /root/flytekit.config serialize workflows -f $(SERIALIZED_PB_OUTPUT_DIR) --pkgs $(PACKAGES_TO_SERIALIZE)
 
 .PHONY: register
 register: serialize
